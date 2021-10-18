e6c7be55fdb8a434ef6023e44bfd82f152ebad0f
Lucas Edgar Cardoso <83605600+Luscas-PandC@users.noreply.github.com>
2021-10-18 16:13:05 -0300

    Movendo os components pra uma pasta especifica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './component/home/home.component';
-import { DonateComponent } from './component/donate/donate.component';
-import { UserComponent } from './component/user/user.component';
-import { PetComponent } from './component/pet/pet.component';
-import { ReportPageComponent } from './component/report-page/report-page.component';
-import { PetMenuComponent } from './component/pet-menu/pet-menu.component';
-import { AdoptPageComponent } from './component/adopt-page/adopt-page.component';
-import { PetPageComponent } from './component/pet-page/pet-page.component';
-import { UserMenuComponent } from './component/user-menu/user-menu.component';
-import { UserLoginComponent } from './component/user-login/user-login.component';
+import { HomeComponent } from './components/home/home.component';
+import { DonateComponent } from './components/donate/donate.component';
+import { UserComponent } from './components/user/user.component';
+import { PetComponent } from './components/pet/pet.component';
+import { ReportPageComponent } from './components/report-page/report-page.component';
+import { PetMenuComponent } from './components/pet-menu/pet-menu.component';
+import { AdoptPageComponent } from './components/adopt-page/adopt-page.component';
+import { PetPageComponent } from './components/pet-page/pet-page.component';
+import { UserMenuComponent } from './components/user-menu/user-menu.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
